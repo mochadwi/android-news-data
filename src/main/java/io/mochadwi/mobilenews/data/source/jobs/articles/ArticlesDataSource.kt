c4c47ee6b093c7a92b3f88e5214606947e713b9a
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-05-08 16:58:13 +0700

    [ADD] ArticlesDataSource #01

---
+++ src/main/java/io/mochadwi/mobilenews/data/source/jobs/articles/ArticlesDataSource.kt
@@ -0,0 +1,18 @@
+package io.mochadwi.mobilenews.data.source.jobs.articles
+
+import io.mochadwi.mobilenews.domain.model.Product
+import io.mochadwi.mobilenews.domain.model.articles.ArticlesModel
+
+/**
+ * Created by irfanirawansukirman on 22/03/18.
+ */
+interface ArticlesDataSource {
+
+    fun getArticles(callback: GetArticlesCallback)
+    fun clearDisposable()
+
+    interface GetArticlesCallback {
+        fun onProductsLoad(articles: ArticlesModel)
+        fun onFailed(errorMessage: String)
+    }
+}
